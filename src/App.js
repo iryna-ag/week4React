33382c06c93350d3894a55d6062c1e5d58fc21f3
iryna-ag <iryna.agafonov@gmail.com>
2022-04-20 00:49:41 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        Weather react
         <Weather defaultCity="Simferopol" />
         <footer>
           <small>
